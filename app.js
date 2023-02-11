fbc20d839ac091d3c64492cdee6615b46b777b70
Daniele Fruciano <danielefruciano@Mac-mini.local>
2023-02-11 07:32:38 -0500

    finished Functions deliverable

--- app.js
+++ app.js
@@ -1,70 +1,129 @@
 // 1.1. What is the difference between a parameter and an argument?
-
+// A parameter is what is in parentheses, passed within the function. Argument is when you call the function. Parameter is data getting passed in the argument
 // 1.2. Within a function, what is the difference between return and console.log?
-
+// printed the result to console, return ends the execution of the function
 // 1. 3. What are the implications of the ability of a function to return a value?
-
+// 
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
+    return num ** 3
 }
+console.log(calculateCube(2));
 
 // 3. isAVowel
 function isAVowel(letter) {
-    // YOUR CODE HERE
+    return "aeiou".includes(letter.toLowerCase());
 }
-
-// 4. getTwoLengths
+console.log(isAVowel("b"));
+// // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
+    return [word1.length, word2.length];
 }
-
+console.log(getTwoLengths("Elizabeth", "Daniele"))
 // 5. sumArray
 function sumArray(arr) {
-    // YOUR CODE HERE
+    let sum = arr.reduce((acc, curr) => acc + curr);
+    return sum;
+//     let sum = 0
+//     for (let i = 0; i<arr.length; i++) {
+//         sum += arr[i]}
+
+//         return sum;
 }
+console.log(sumArray([1,2,3,4,5,6]))
 
 // 6.1 checkPrime
 function checkPrime(num) {
-    // YOUR CODE HERE
+    for (let i = 2; i < Math.sqrt (num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
-
-// 6.2 printPrimes
-function printPrimes(num) {
-    // YOUR CODE HERE
+console.log(checkPrime(24))
+// // 6.2 printPrimes
+function checkPrimes(num) {
+    for (let i = 2; i <= num; i++) {
+        if (checkPrime(i) === true) {
+            console.log(i);
+        }
+    }
 }
+console.log(checkPrimes(20))
 
 // 7. printLongestWord
 function printLongestWord(arr) {
-    // YOUR CODE HERE
+//     let longestWord = "";
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].length > longestWord.length) {
+//             longestWord = arr[i]
+//         }
+//     }
+//     return longestWord 
+// }
+// or as written below with the accumulator and current .reduce method, note on line 71 it checks for a string because default the accumulator by default starts at 0
+
+let longestWord = arr.reduce((acc, curr) => {
+    if (curr.length > acc.length) {
+        acc = curr;
+    } return acc;
+ }, "")
+    return longestWord;
 }
 
-// BONUS!
+console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "blob"]));
+// // BONUS!
 
 // 8. eulerFibo
-function eulerFibo(num) {
-    // YOUR CODE HERE
-}
+// function eulerFibo(num) {
+//    let x = 1;
+//    let y =2;
+//    let z = null;
+//    while (y <= num) ; {
+//    if (y % 2 == 0) {
+//     sum += y;
+//    }
+//    z = x + y;
+//    x = y;
+//    y = z; 
+// } return sum
+// }
+// console.log(eulerFibo(4000000));
 
 // 9. findNeedle
 function findNeedle(arr) {
-    // YOUR CODE HERE
+//     return (`found the needle at position ${arr.indexOf("needle")}`)
+// }
+// or can also be solved using a for loop
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].toLowerCase() === "needle") {
+        return (`found the needle at position ${i}`)
+    }
 }
+}
+console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']));
 
 // 10. sumPositive
 function sumPositive(arr) {
-    // YOUR CODE HERE
+    let sum = arr.reduce((acc, curr) => {
+        if (curr > 0) {
+            acc += curr
+        }
+        return acc
+    }, 0)
+    return sum
 }
-
-module.exports = {
-    calculateCube,
-    isAVowel,
-    getTwoLengths,
-    sumArray,
-    checkPrime,
-    printPrimes,
-    printLongestWord,
-    eulerFibo,
-    findNeedle,
-    sumPositive
-};
+console.log(sumPositive([1, -4, 7, 12]))
+// module.exports = {
+//     calculateCube,
+//     isAVowel,
+//     getTwoLengths,
+//     sumArray,
+//     checkPrime,
+//     printPrimes,
+//     printLongestWord,
+//     eulerFibo,
+//     findNeedle,
+//     sumPositive
+// };
